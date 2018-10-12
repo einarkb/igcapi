a1aad9f534be1edfb1e8a95940e810ca3662d8ac
Einar Budsted <budsted@hotmail.com>
2018-10-12 02:14:29 +0200

    test

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/marni/goigc"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +17,15 @@ func main() {
 	if port == "" {
 		log.Fatal("$PORT is not set")
 	}
+
+	track, err := igc.ParseLocation("http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc")
+	if err != nil {
+		fmt.Errorf("Problem reading the track", err)
+	} else {
+		fmt.Printf("Pilot: %s, gliderType: %s, date: %s",
+			track.Pilot, track.GliderType, track.Date.String())
+	}
+
 	http.HandleFunc("/", hello)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		panic(err)
