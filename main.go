a7c285cfe57de3144766a3add459d7a88fa67514
Einar Budsted <budsted@hotmail.com>
2018-10-12 02:22:38 +0200

    test

--- main.go
+++ main.go
@@ -11,19 +11,18 @@ import (
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello World")
+	track, err := igc.ParseLocation("http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc")
+	if err != nil {
+		fmt.Errorf("Problem reading the track", err)
+	} else {
+		fmt.Fprintf(w, "Pilot: %s, gliderType: %s, date: %s",
+			track.Pilot, track.GliderType, track.Date.String())
+	}
 }
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("$PORT is not set")
-	}
-
-	track, err := igc.ParseLocation("http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc")
-	if err != nil {
-		fmt.Errorf("Problem reading the track", err)
-	} else {
-		fmt.Printf("Pilot: %s, gliderType: %s, date: %s",
-			track.Pilot, track.GliderType, track.Date.String())
 	}
 
 	http.HandleFunc("/", hello)
