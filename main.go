078a6a55b12035e3bde12ddb613abf3230b5c370
Einar Budsted <budsted@hotmail.com>
2018-10-12 07:13:51 +0200

    debugging

--- main.go
+++ main.go
@@ -34,11 +34,11 @@ func IgcHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		var input Input
 		json.NewDecoder(r.Body).Decode(&input)
-		/*if id := globalTracksDb.Add(input.URL); id > 0 {
-			json.NewEncoder(w).Encode(ID{id})
-			return
-		}*/
-		fmt.Fprintf(w, "The URL already exists")
+		//if id := globalTracksDb.Add(input.URL); id > 0 {
+		json.NewEncoder(w).Encode(ID{5})
+		return
+		//}
+		//fmt.Fprintf(w, "The URL already exists")
 	}
 	/*switch r.Method {
 	case "POST":
