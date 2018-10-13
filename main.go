b737f16289e2d009860bb43cd62822884dc878ad
Einar Budsted <budsted@hotmail.com>
2018-10-13 19:56:55 +0200

    cleaning up a bit

--- main.go
+++ main.go
@@ -55,75 +55,43 @@ func IgcHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	} else if r.Method == "GET" {
 		parts := strings.Split(r.URL.Path, "/")
-
-		i, err := strconv.Atoi(parts[4])
+		if len(parts) != 5 {
+			http.Error(w, "Invalid request", http.StatusBadRequest)
+			return
+		}
+		id, err := strconv.Atoi(parts[4])
 		if err != nil {
 			if parts[4] == "" {
-				ids := globalTracksDb.GetIDs()
-				json.NewEncoder(w).Encode(ids)
+				ReplyWithAllTrackIds(w)
 				return
 			}
-
 			http.Error(w, "Invalid url", http.StatusBadRequest)
 			return
 		}
-		type Input struct { // temp
-			URL string `json:"url"`
-		}
-		// check if int
-		trackURL, found := globalTracksDb.Get(i)
-		if !found {
-			http.Error(w, "No track found with id: "+strconv.Itoa(i), http.StatusBadRequest)
-			return
-		}
-		track, err := igc.ParseLocation(trackURL)
-		if err != nil {
-			fmt.Errorf("Problem reading the track", err)
-		} else {
-			fmt.Fprintf(w, "Pilot: %s, gliderType: %s, date: %s",
-				track.Pilot, track.GliderType, track.Date.String())
-		}
-		//json.NewEncoder(w).Encode(Input{track})
+		ReplyWithTrack(w, id)
 	}
-	/*switch r.Method {
-	case "POST":
-		type Input struct {
-			URL string `json:"url"`
-		}
-		var input Input
-		err := json.NewDecoder(r.Body).Decode(&input)
-		switch err {
-		case nil:
-			if input.URL == "" {
-				http.Error(w, "No url in body", http.StatusBadRequest)
-			} else {
-				//check if valid igc url
-				if id := globalTracksDb.Add(input.URL); id > 0 {
-					fmt.Fprintf(w, "{\"id\": "+strconv.Itoa(id)+"}")
-					return
-				}
-				fmt.Fprintf(w, "The URL already exists")
-			}
-			fmt.Fprintf(w, "heeei")
-		case io.EOF:
-			http.Error(w, "POST body is empty", http.StatusBadRequest)
-		default:
-			http.Error(w, "Something went wrong", http.StatusInternalServerError)
-			fmt.Fprintf(w, err.Error())
-		}
+}
 
-	case "GET":
-		type IDStruct struct {
-			ID int `json:"id"`
-		}
-		for _, v := range globalTracksDb.ids {
-			idStruct := IDStruct{v}
-			json.NewEncoder(w).Encode(idStruct)
-		}
-	default:
-		fmt.Fprintf(w, "not post")
-	}*/
+// ReplyWithTrack replies with the track of specified id
+func ReplyWithTrack(w http.ResponseWriter, id int) {
+	trackURL, found := globalTracksDb.Get(id)
+	if !found {
+		http.Error(w, "No track found with id: "+strconv.Itoa(id), http.StatusBadRequest)
+		return
+	}
+	track, err := igc.ParseLocation(trackURL)
+	if err != nil {
+		fmt.Errorf("Problem reading the track", err)
+	} else {
+		fmt.Fprintf(w, "Pilot: %s, gliderType: %s, date: %s",
+			track.Pilot, track.GliderType, track.Date.String())
+	}
+}
 
+// ReplyWithAllTrackIds replies with an array of all track ids
+func ReplyWithAllTrackIds(w http.ResponseWriter) {
+	ids := globalTracksDb.GetIDs()
+	json.NewEncoder(w).Encode(ids)
 }
 
 var globalTracksDb TrackURLsDB
